d4fa39f26fdb7e8f7839f3dc98eec0d539d145d1
Joao Salazar <joaobosco.salazar@gmail.com>
2020-12-10 14:38:26 +0000

    Corrigindo os Assets e o WebConsole

---
+++ app/javascript/packs/users_backoffice.js
@@ -0,0 +1,8 @@
+//= require jquery-2.2.3/dist/jquery
+//= require bootstrap/dist/js/bootstrap
+//= require fastclick/lib/fastclick
+//= require nprogress/nprogress
+//= require bootstrap-growl-ifightcrime/jquery.bootstrap-growl
+//= require jquery-ui/ui/widgets/datepicker
+//= require jquery-ui/ui/i18n/datepicker-pt-BR
+//= require custom
